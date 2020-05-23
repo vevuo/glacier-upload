8cf6ba8b6d7a1b7006b4cda1893a4f7dfb67bee1
vevuo <vesa.vuoristo@gmail.com>
2020-05-23 21:00:02 +0300

    initial commit

---
+++ glacier_backup/glacier/glacier_library.py
@@ -0,0 +1,7 @@
+import boto3
+from botocore.utils import calculate_tree_hash, calculate_sha256
+
+
+class Glacier:
+    def __init__(self):
+        pass
---
+++ glacier_backup/main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import argparse
+from glacier.glacier_library import Glacier
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser(
+        prog='glacier_backup',
+        usage='%(prog)s [options] instance folder',
+        description='Backup files to AWS Glacier',
+        epilog='Happy backuping!'
+        )
+    parser.add_argument(
+        'folder',
+        metavar='folder',
+        type=str,
+        help='Folder holding files for backup'
+        )        
+    parser.add_argument(
+        'instance',
+        metavar='instance',
+        type=str,
+        help='AWS Glacier instance name'
+        )
+    parser.add_argument(
+        '-s',
+        '--size',
+        default='32000',
+        type=int,
+        help='Size of the sent packages',
+        )
+    return parser
+
+
+def main():
+    parser = setup_parser()
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
